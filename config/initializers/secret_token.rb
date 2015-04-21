b27a6b6271e520c8442434b466d96a7a94bf74bb
avega2991 <rambler2991@rambler.ru>
2015-04-21 16:34:56 +0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EdxFirst::Application.config.secret_token = '5994f9574c2a1e0e70c0905360c2ed19b518c45caeb7dd7c0a264b2356ac5a803c2cb15b0d32c638fb8097b73a6744d21b8d03c09417ec7f5fd21787d0f1cec5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+EdxFirst::Application.config.session_store :cookie_store, key: '_edx_first_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# EdxFirst::Application.config.session_store :active_record_store
